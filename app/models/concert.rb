4ef4e646ddd5be846d2a85084309cf7f13a956db
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-09-18 11:35:25 +0300

    initialize concert date, band and venue

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Concert
+    attr_accessor :date, :band,  :venue
+    @@all= []
+
+    def initialize(date, band, venue)
+        @date = date
+        @band = band
+        @venue = venue
+        @@all << self
+        
+    end
+
+    def self.all
+        @@all
+        
+    end
     
 end
